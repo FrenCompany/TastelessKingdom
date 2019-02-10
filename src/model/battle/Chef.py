6d84d7e5a488c0015758b03a25509c62dfc744f4
Tomas Ahumada <tomyahu@gmail.com>
2019-02-10 03:59:22 -0300

    Cook method.

--- src/model/battle/Chef.py
+++ src/model/battle/Chef.py
@@ -1,11 +1,15 @@
 # Player
 from .Inventory import *
+from .Citizen import Citizen
+from .Dish import Dish
+from typing import List
 
 
 class Chef:
     def __init__(self, player_deck, player_inventory, player_specialty, player_modifier, player_defense,
                  cooldown,
-                 max_morale):
+                 max_morale,
+                 citizens: List[Citizen]):
         self.deck = player_deck
         self.inventory = Inventory(player_inventory)
         self.specialty = player_specialty
@@ -14,10 +18,21 @@ class Chef:
         self.cooldown = cooldown
         self.max_morale = max_morale
         self.current_morale = max_morale
+        self.citizens = citizens
 
     def cook(self, recipe):
-        for ing in recipe.ingredient_list:
-            self.inventory.use_item(ing)
+        if self.inventory.can_cook(recipe):
+            for ing in recipe.ingredient_list:
+                self.inventory.use_item(ing)
+
+            dish = Dish(self, recipe.attributes, recipe.powers)
+
+            for citizen in self.citizens:
+                citizen.eat(dish)
+
+            return dish
+        else:
+            raise Exception("Not enough ingredients")
 
     def lower_morale(self, value):
         self.current_morale -= value
