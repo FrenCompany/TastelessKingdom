dc6d9ff086a8e63d8114ff5c9715eda8ce3b7269
Mati <matilde.rivas@ug.uchile.cl>
2019-02-10 03:32:47 -0300

    Implements Chef's inventory in battle mode

--- src/model/battle/Chef.py
+++ src/model/battle/Chef.py
@@ -5,7 +5,7 @@ from .Inventory import *
 class Chef:
     def __init__(self, player_deck, player_inventory, player_specialty, player_modifier, player_defense,
                  cooldown,
-                 max_morale, current_morale):
+                 max_morale):
         self.deck = player_deck
         self.inventory = Inventory(player_inventory)
         self.specialty = player_specialty
@@ -16,7 +16,8 @@ class Chef:
         self.current_morale = max_morale
 
     def cook(self, recipe):
-        pass
+        for ing in recipe.ingredient_list:
+            self.inventory.use_item(ing)
 
     def lower_morale(self, value):
         self.current_morale -= value
--- src/model/battle/Inventory.py
+++ src/model/battle/Inventory.py
@@ -1,7 +1,24 @@
 class Inventory:
     # takes the player's basic inventory (a dictionary with the enum values as keys,
     # the quantity and regeneration of each ingredient) and creates the inventory needed
-    # for battle
+    # for battle, with available ingredients
 
     def __init__(self, player_inventory):
-        pass
+        self.inventory = {}
+        for i in player_inventory:
+            item = player_inventory[i]
+            # inventory: max, regeneration ratio, available
+            self.inventory[i] = (item[0], item[1], item[0])
+
+    # regenerates ingredients according to their regeneration ratio
+    def regenerate_ingredients(self):
+        for i in self.inventory:
+            item = self.inventory[i]
+            # if item isn't fully stocked
+            if item[2] < item[0]:
+                # adds corresponding portion
+                item[2] += item[1]
+
+
+    def use_item(self, item):
+        self.inventory[item][2] -= 1
