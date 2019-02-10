e0231f5f87b723c00d49a9ae5d54f77f6bbe355d
TakeItIsi <isidora.chuo@hotmail.cl>
2019-02-10 03:59:26 -0300

    Mas cosas de dialogos

--- src/model/NPC.py
+++ src/model/NPC.py
@@ -2,13 +2,14 @@ from Lib.copy import deepcopy
 from src.model import Dialogue
 
 class NPC:
-    def __init__(self, name, dialogues, afterBattle):
+    def __init__(self, name, dialogues=[], afterBattle, animatedsprite):
         self.friend=False #maybe not necessary to set in init
         self.name=name #string (duh)
         self.dialogueState=0 (int)
         self.dialogues=dialogues #list of Dialogue objects
         self.afterBattleDialogue=afterBattle
         self.battleState=False
+        self.sprite=animatedsprite
     def getName(self):
         return self.name
 
@@ -17,7 +18,8 @@ class NPC:
 
     def getName(self):
         return self.name
-
+    def setDialogue(self,nuevo):
+        self.dialogues.append(nuevo)
     def getDialogue(self):
         if self.battleState==False:
             return self.dialogues[self.dialogueState].getDialogue()
@@ -26,7 +28,7 @@ class NPC:
             return self.afterBattleDialogue.getDialogue()
 
     def nextState(self):
-        if self.dialogueState<len(self.dialogues):
+        if self.dialogueState<len(self.dialogues) and self.dialogues[self.dialogueState].cond:
             self.dialogueState+=1
 
     def setBattleState(self, didYouWin):
