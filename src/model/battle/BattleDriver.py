ea1819bbdd3c75397f7b9033a8fb58f610b208d1
Franco Cruces <francocrucesayala@gmail.com>
2019-02-10 13:21:01 -0300

    Driver citizen refactor

--- src/model/battle/BattleDriver.py
+++ src/model/battle/BattleDriver.py
@@ -1,20 +1,21 @@
+from typing import List
+from .Citizen import Citizen
 from .Chef import Chef
 from .BattleState.ChefTurn import ChefTurn
 
 
 class BattleDriver:
-    def __init__(self, player, citizens):
+    def __init__(self, player, citizens: List[Citizen]):
         # TODO: implement player class
         self.chef = Chef(player.deck, player.inventory, player.speciality, player.modifier, player.defense,
-                         player.max_morale, citizens)
+                         player.max_morale)
+        self.citizens = citizens
         self.state = ChefTurn(self)
 
     def set_state(self, state):
         self.state = state
 
-    def battle_loop(self):
-        # Tick buffs
-        # Chef turn (cook)
-        # Citizen turn (eat and react)
-        # End battle?
-        pass
+    def recipe_chosen(self, recipe):
+        dish = self.chef.cook(recipe)
+        for citizen in self.citizens:
+            citizen.eat(dish)
--- src/model/battle/Chef.py
+++ src/model/battle/Chef.py
@@ -7,8 +7,7 @@ from typing import List
 
 class Chef:
     def __init__(self, player_deck, player_inventory, player_specialty, player_modifier, player_defense,
-                 max_morale,
-                 citizens: List[Citizen]):
+                 max_morale):
         self.deck = player_deck
         self.inventory = Inventory(player_inventory)
         self.specialty = player_specialty
@@ -16,19 +15,12 @@ class Chef:
         self.defense = player_defense
         self.max_morale = max_morale
         self.current_morale = max_morale
-        self.citizens = citizens
 
     def cook(self, recipe):
         if self.inventory.can_cook(recipe):
             for ing in recipe.ingredient_list:
                 self.inventory.use_item(ing)
-
-            dish = Dish(self, recipe.attributes, recipe.powers)
-
-            for citizen in self.citizens:
-                citizen.eat(dish)
-
-            return dish
+            return Dish(self, recipe.attributes, recipe.powers)
         else:
             raise Exception("Not enough ingredients")
 
